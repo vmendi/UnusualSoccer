85009f0ed9782ffa794003d181f96540d22f8557
vmendi <vmendi@gmail.com>
2012-02-25 19:20:11 +0100

    -

--- SoccerServer/SoccerServer/App_Data/DeleteAll.sql
+++ SoccerServer/SoccerServer/App_Data/DeleteAll.sql
@@ -1,10 +1,10 @@
 DELETE FROM [SoccerV2].[dbo].[Players]
 GO
 
-DELETE FROM [SoccerV2].[dbo].[CompetitionSeasons]
+DELETE FROM [SoccerV2].[dbo].[Matches]
 GO
 
-DELETE FROM [SoccerV2].[dbo].[MatchParticipations] 
+DELETE FROM [SoccerV2].[dbo].[CompetitionSeasons]
 GO
 
 DELETE FROM [SoccerV2].[dbo].[Purchases] 
