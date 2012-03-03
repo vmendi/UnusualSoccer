83b09db13eb8135cd3c75921f800f5a509832cd6
vmendi <vmendi@gmail.com>
2012-03-03 22:15:06 +0100

    -

---
+++ SoccerServer/SoccerServer/Ping.ashx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NLog;
+
+namespace SoccerServer
+{
+    public class Ping : IHttpHandler
+    {
+        private static readonly Logger Log = LogManager.GetLogger(typeof(Ping).FullName);
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Pong");
+
+            Log.Debug("Pong from " + context.Server.MachineName);
+        }
+
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+    }
+}
