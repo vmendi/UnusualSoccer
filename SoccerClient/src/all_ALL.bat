30845648f2931815a381fbe9a93cb52fec06613e
Santiago Revelo <santipikanti@gmail.com>
2012-08-27 19:16:59 +0200

    -

--- SoccerClient/src/all_ALL.bat
+++ SoccerClient/src/all_ALL.bat
@@ -1,5 +1,5 @@
 @echo off
 path %path%;%1
-mxmlc -locale=en_US -source-path=./, -source-path+=../locale/{locale} -include-resource-bundles=match -output ../../SoccerServer/SoccerServer/Imgs/Match_en_US.swf
 mxmlc -locale=es_ES -source-path=./, -source-path+=../locale/{locale} -include-resource-bundles=match -output ../../SoccerServer/SoccerServer/Imgs/Match_es_ES.swf
+mxmlc -locale=en_US -source-path=./, -source-path+=../locale/{locale} -include-resource-bundles=match -output ../../SoccerServer/SoccerServer/Imgs/Match_en_US.swf
 mxmlc -locale=es_LA -source-path=./, -source-path+=../locale/{locale} -include-resource-bundles=match -output ../../SoccerServer/SoccerServer/Imgs/Match_es_LA.swf
